b83eae499a1c690c0e5241bee091e411b4749f59
sravan1780 <panchakotisravankumar000@gmail.com>
2025-03-04 23:21:02 +0530

    removed demo file

--- demo.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key="AIzaSyCKjQxgtWj-fG77rwpPe3wNT2xljUEleDU")
-
-models = genai.list_models()
-for model in models:
-    print(model.name)
